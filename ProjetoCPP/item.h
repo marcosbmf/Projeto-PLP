93c5f5c64c380d898559933055982bbe3d37e186
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2018-05-23 14:12:01 -0300

    erros de compilação corrigidos

--- ProjetoCPP/item.h
+++ ProjetoCPP/item.h
@@ -2,43 +2,46 @@
 #include <sstream>
 #include <string>
 
-typedef struct Item {
-  std::string nome;
-  std::string descricao;
-  double preco;
-} Item;
+namespace item {
+  typedef struct Item {
+    std::string nome;
+    std::string descricao;
+    double preco;
+  } Item;
 
-/**
-  * "Constrói" novo Item.
-  */
-Item newItem(){
-  Item item;
+  /**
+    * "Constrói" novo Item.
+    */
+  Item newItem(){
+    Item item;
 
-  std::cout << "Digite o nome do produto: ";
-  std::getline(std::cin, item.nome);
-  std::cout << "Digite a descrição do produto: ";
-  std::getline(std::cin, item.descricao);
-  std::cout << "Digite o preço: ";
-  std::cin >> item.preco;
-  std::cin.get();
+    std::cout << "Digite o nome do produto: ";
+    std::getline(std::cin, item.nome);
+    std::cout << "Digite a descrição do produto: ";
+    std::getline(std::cin, item.descricao);
+    std::cout << "Digite o preço: ";
+    std::cin >> item.preco;
+    std::cin.get();
 
-  return item;
-}
+    return item;
+  }
 
-/**
-  * Retorna descrição simplificada do item passado como argumento.
-  */
-std::string listaItem(Item item) {
-  std::string saida = item.nome << " - R$ " << item.preco;
-  return saida;
-}
+  /**
+    * Retorna descrição simplificada do item passado como argumento.
+    */
+  std::string listaItem(Item item) {
+    std::ostringstream saida;
+    saida << item.nome << " - R$ " << std::to_string(item.preco);
+    return saida.str();
+  }
 
-/**
-  * Retorna descrição detalhada do item passado como argumento.
-  */
-std::string toString(Item item) {
-  std::string saida = "";
-  saida << item.nome << " - R$ " << item.preco << "\n";
-  saida << "Descrição: " << item.descricao;
-  return saida;
+  /**
+    * Retorna descrição detalhada do item passado como argumento.
+    */
+  std::string toString(Item item) {
+    std::ostringstream saida;
+    saida << item.nome << " - R$ " << std::to_string(item.preco) << std::endl;
+    saida << "Descrição: " << item.descricao;
+    return saida.str();
+  }
 }
