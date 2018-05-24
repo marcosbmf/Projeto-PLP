b3e1b931c42515db9307a0237c9e0860f6b5338c
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2018-05-24 20:48:49 -0300

    Menu corrigido

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include "models/item.h"
+#include "models/pedido.h"
 #include "models/restaurant.h"
 #include "models/client.h"
-#include "models/pedido.h"
 #include "models/menuCliente.h"
 
 /*
@@ -14,208 +15,220 @@
     fazer login como cliente ou restaurante
 */
 
-void cadastrarNovoCliente(std::vector<clt::Client> &clientes) {
+std::string msgSistema = "";
+std::string icnSistema = "_";
+std::string tituloSessao = "SISTEMA DE DELIVERY";
+const std::string SUCESSO = "_";
+const std::string ERRO = "!";
 
+void limpaTela() {
     system("clear");
-    bool cadastroValido = true;
-
-    std::cout << "===> Cadastrar Novo Cliente <===\n";
-    clt::Client cliente = clt::newCliente(); 
-
-    for (auto it = clientes.begin(); it != clientes.end(); ++it) {
-        if (it->login == cliente.login) {
-            cadastroValido = false;
-        } 
-    }
-
-    if (cadastroValido) {
-        clientes.push_back(cliente);
-        std::cout << "======== CADASTRO EFETUADO COM SUCESSO :D =========\n\n";
-    } else {
-        std::cout << "!!!!!!!!! ERRO NO CADASTRO: LOGIN JA EXISTE !!!!!!!!!";
-    }
-}
-
-void cadastrarNovoRestaurante(std::vector<rst::Restaurant> &restaurantes) {
-
-    system("clear");
-    std::cout << "===> Cadastrar Novo Restaurante <===\n";
-    bool cadastroValido = true;
-    rst::Restaurant r = rst::newRestaurant();
-
-    for (auto it = restaurantes.begin(); it != restaurantes.end(); ++it) {
-        if (it->cnpj == r.cnpj) {
-            cadastroValido = false;
-        } 
-    }
-
-    if (cadastroValido) {
-        restaurantes.push_back(r);
-        std::cout << "======== CADASTRO EFETUADO COM SUCESSO :D =========\n\n";
-    } else {
-        std::cout << "!!!!!!!!! ERRO NO CADASTRO: CNPJ JA EXISTE !!!!!!!!!";
-    }
+    std::cout << "[" << icnSistema << "] " << msgSistema << std::endl;
+    std::cout << "----------------------------------------------------------------" << std::endl;
+    std::cout << tituloSessao << std::endl;
+    std::cout << "----------------------------------------------------------------" << std::endl << std::endl;
 
 }
 
-int loginCliente(std::vector<clt::Client> clientes, std::string login, std::string senha){
+void titulo(std::string titulo) {
 
-    for (auto it = clientes.begin(); it != clientes.end(); it++) {
-        if (it->login == login) {
-            if (it->password == senha) {
-                return 1;
-            } else {
-                std::cout << "!!!!!!!!! ERRO NO LOGIN: SENHA INCORRETA !!!!!!!!!";
-                return 0;
+}
+
+namespace fn {
+    void cadastrarNovoCliente(std::vector<clt::Client> &clientes) {
+        icnSistema = SUCESSO;
+        msgSistema = "";
+        tituloSessao = "CADASTRAR NOVO CLIENTE";
+        limpaTela();
+
+        bool cadastroValido = true;
+        clt::Client cliente = clt::newCliente();
+
+        for (auto it = clientes.begin(); it != clientes.end(); ++it) {
+            if (it->login == cliente.login) {
+                cadastroValido = false;
+            }
+        }
+
+        if (cadastroValido) {
+            clientes.push_back(cliente);
+            icnSistema = SUCESSO;
+            msgSistema = "Cadastro efetuado com sucesso.";
+
+        } else {
+            icnSistema = ERRO;
+            msgSistema = "Erro ao cadastrar: login ja existe.";
+        }
+    }
+
+    void cadastrarNovoRestaurante(std::vector<rst::Restaurant> &restaurantes) {
+        icnSistema = SUCESSO;
+        msgSistema = "";
+        tituloSessao = "CADASTRAR NOVO RESTAURANTE";
+        limpaTela();
+
+        bool cadastroValido = true;
+        rst::Restaurant r = rst::newRestaurant();
+
+        for (auto it = restaurantes.begin(); it != restaurantes.end(); ++it) {
+            if (it->cnpj == r.cnpj) {
+                cadastroValido = false;
+            }
+        }
+
+        if (cadastroValido) {
+            restaurantes.push_back(r);
+            icnSistema = SUCESSO;
+            msgSistema = "Cadastro efetuado com sucesso.";
+        } else {
+            icnSistema = ERRO;
+            msgSistema = "Erro no cadastro: CNPJ ja existe.";
+        }
+
+    }
+
+    int loginCliente(std::vector<clt::Client> clientes, std::string login, std::string senha){
+
+        for (auto it = clientes.begin(); it != clientes.end(); it++) {
+            if (it->login == login) {
+                if (it->password == senha) {
+                    return 1;
+                } else {
+                    icnSistema = ERRO;
+                    msgSistema = "Erro no login: senha incorreta.";
+                    return 0;
+                }
+            }
+        }
+
+        icnSistema = ERRO;
+        msgSistema = "Erro no login: usuario nao existe.";
+        return 0;
+    }
+
+    int loginRestaurante(std::vector<rst::Restaurant> restaurantes, std::string cnpj, std::string senha){
+
+        for (auto it = restaurantes.begin(); it != restaurantes.end(); it++) {
+            if (it->cnpj == cnpj) {
+                if (it->password == senha) {
+                    return 1;
+                } else {
+                    icnSistema = ERRO;
+                    msgSistema = "Erro no login: senha incorreta.";
+                    return 0;
+                }
+            }
+        }
+
+        icnSistema = ERRO;
+        msgSistema = "Erro no login: usuario nao encontrado.";
+        return 0;
+    }
+
+    clt::Client * getClient(std::string login, std::vector<clt::Client> &clientes){
+        for (auto it = clientes.begin(); it != clientes.end(); it++) {
+            if (it->login == login) {
+                return &*it;
             }
         }
     }
-    
-    std::cout << "!!!!!!!!! ERRO NO LOGIN: USUARIO NAO EXISTE !!!!!!!!!";
-    return 0;
-}
 
-int loginRestaurante(std::vector<rst::Restaurant> restaurantes, std::string cnpj, std::string senha){
-
-    for (auto it = restaurantes.begin(); it != restaurantes.end(); it++) {
-        if (it->cnpj == cnpj) {
-            if (it->password == senha) {
-                return 1;
-            } else {
-                std::cout << "!!!!!!!!! ERRO NO LOGIN: SENHA INCORRETA !!!!!!!!!";
-                return 0;
+    rst::Restaurant * getRestaurante(std::string cnpj, std::vector<rst::Restaurant> &restaurantes){
+        for (auto it = restaurantes.begin(); it != restaurantes.end(); it++) {
+            if (it->cnpj == cnpj) {
+                return &*it;
             }
         }
     }
-    
-    std::cout << "!!!!!!!!! ERRO NO LOGIN: USUARIO NAO EXISTE !!!!!!!!!";
-    return 0;
 }
 
-clt::Client * getClient(std::string login, std::vector<clt::Client> &clientes){
-    for (auto it = clientes.begin(); it != clientes.end(); it++) {
-        if (it->login == login) {
-            return &*it;
+namespace menu {
+    void printMenu() {
+        std::cout << "MENU:" << std::endl << std::endl
+                  << "(1) Login (cliente)" << std::endl
+                  << "(2) Login (restaurante)" << std::endl
+                  << "(3) Cadastrar-se (cliente)" << std::endl
+                  << "(4) Cadastrar-se (restaurante)" << std::endl
+                  << "(5) Sair" << std::endl << std::endl;
+    }
+
+    void loginClt(std::vector<clt::Client> &clientes, std::vector<rst::Restaurant> &restaurantes) {
+        limpaTela();
+        std::string login, senha;
+
+        std::cout << "Nome de usuario: ";
+        std::getline(std::cin, login);
+        std::cout << "Senha: ";
+        std::getline(std::cin, senha);
+
+        if (fn::loginCliente(clientes, login, senha)){
+            icnSistema = SUCESSO;
+            msgSistema = "Sucesso no login.";
+            menuCliente(restaurantes, *fn::getClient(login, clientes));
         }
     }
-}
 
-rst::Restaurant * getRestaurante(std::string cnpj, std::vector<rst::Restaurant> &restaurantes){
-    for (auto it = restaurantes.begin(); it != restaurantes.end(); it++) {
-        if (it->cnpj == cnpj) {
-            return &*it;
+    void loginRst(std::vector<rst::Restaurant> &restaurantes) {
+        limpaTela();
+        std::string cnpj;
+        std::string senha;
+        std::cout << "CNPJ: ";
+        std::getline(std::cin, cnpj);
+        std::cout << "Senha: ";
+        std::getline(std::cin, senha);
+
+        if (fn::loginRestaurante(restaurantes, cnpj, senha)) {
+            icnSistema = SUCESSO;
+            msgSistema = "Sucesso no login";
+            //menuRestaurante(cnpj);
         }
     }
 }
 
 int main() {
+    const int LOGIN_CLT = 1;
+    const int LOGIN_RST = 2;
+    const int CDST_CLT = 3;
+    const int CDST_RST = 4;
+    const int SAIR = 5;
 
     std::vector<clt::Client> clientes;
     std::vector<rst::Restaurant> restaurantes;
+    int opMenuInicial = 6;
 
-    std::string opMenuInicial;
+    while (opMenuInicial != SAIR) {
+        std::string entrada;
+        tituloSessao = "SISTEMA DE DELIVERY";
+        limpaTela();
+        menu::printMenu();
+        std::cout << "> ";
+        std::cin >> entrada;
+        opMenuInicial = std::stoi(entrada);
 
-    while(opMenuInicial != "5") {
+        switch (opMenuInicial) {
+            case LOGIN_CLT:
+                menu::loginClt(clientes, restaurantes);
+                break;
 
-        std::cout <<  "\n\nSeja bem vindo ao Sistema de Delivery."
-        << "\n\n=====> Digite uma opcao para continuar <====="
-        << "\n\n- Fazer login com,o Cliente ---->         (  1  )"
-        << "\n- Fazer login como Restaurante ---->      (  2  )"
-        << "\n- Criar nova conta como Cliente ---->     (  3  )"
-        << "\n- Criar nova conta como Restaurante ----> (  4  )\n"
-        << "\n- Sair ---->                              (  5  )\n";
-        std::cin >> opMenuInicial;
-        
-        if (opMenuInicial == "1") {
+            case LOGIN_RST:
+                menu::loginRst(restaurantes);
+                break;
 
-            std::string login;
-            std::string senha;
-          
-            std::cout << "\n\n Nome de usuario: ";
-            std::cin >> login;
-            std::cout << "\n\n Senha: ";
-            std::cin >> senha;
+            case CDST_CLT:
+                fn::cadastrarNovoCliente(clientes);
+                break;
 
-            if (loginCliente(clientes, login, senha)){
-                std::cout << "Sucesso no login";
-                menuCliente(restaurantes, *getClient(login, clientes));
-            }
+            case CDST_RST:
+                fn::cadastrarNovoRestaurante(restaurantes);
+                break;
 
-        } else if (opMenuInicial == "2") {
-            std::string cnpj;
-            std::string senha;
-          
-            std::cout << "\n\n CNPJ: ";
-            std::cin >> cnpj;
-            std::cout << "\n\n Senha: ";
-            std::cin >> senha;
-
-            if (loginRestaurante(restaurantes, cnpj, senha)){
-                std::cout << "Sucesso no login";
-                //menuRestaurante(cnpj);
-            }
-        } else if (opMenuInicial == "3") {
-            cadastrarNovoCliente(clientes);
-        } else if (opMenuInicial == "4") {
-            cadastrarNovoRestaurante(restaurantes);
+            default:
+                icnSistema = ERRO;
+                msgSistema = "Comando invalido.";
+                break;
         }
 
-        //system("clear");
     }
-    
 
     return 0;
 }
-
-
-
-////////////////////////////ANTIGO MAIN////////////////////////////////////////
-// item::Item coxinha;
-// coxinha.name = "Coxinha";
-// coxinha.price = 2.5;
-// coxinha.description = "God's favourite snack";
-
-// item::Item pastel;
-// pastel.name = "Pastel";
-// pastel.price = 3.0;
-// pastel.description = "Tropical spring roll";
-
-// item::Item alfajor;
-// alfajor.name = "Alfajor";
-// alfajor.price = 3.5;
-// alfajor.description = "Tasty Argentinian cookies";
-
-// rst::Restaurant seuOlavo;
-// seuOlavo.name = "Seu Olavo's";
-// seuOlavo.cnpj = 1;
-// seuOlavo.cuisine = "Fast-food";
-// seuOlavo.phone = "1234-5678";
-// seuOlavo.opensAt[0] = 8;
-// seuOlavo.opensAt[1] = 0;
-// seuOlavo.closesAt[0] = 19;
-// seuOlavo.closesAt[1] = 0;
-// seuOlavo.menu.push_back(coxinha);
-// seuOlavo.menu.push_back(pastel);
-// seuOlavo.menu.push_back(alfajor);
-// std::cout << "Open? " << rst::isOpen(seuOlavo) << std::endl;
-// std::cout << "Menu:" << std::endl << rst::showMenu(seuOlavo) << std::endl << std::endl;
-
-// clt::Client client;
-// client.name = "Melina Mongiovi";
-
-// ord::Order order;
-// rst::Restaurant *sO = &seuOlavo;
-// clt::Client *s1 = &client;
-// order.restaurant = sO;
-// order.client = s1;
-// ord::addItem(order, seuOlavo.menu[0]);
-// ord::addItem(order, seuOlavo.menu[0]);
-// ord::addItem(order, seuOlavo.menu[2]);
-// ord::addItem(order, seuOlavo.menu[2]);
-// ord::removeItem(order, 3);
-// std::cout << "Is sent? " << ord::isSent(order) << std::endl;
-// ord::changeSituation(order, ord::SENT);
-// std::cout << "Is sent? " << ord::isSent(order) << std::endl;
-// std::cout << "Order made by: " << ord::getClient(order).name << std::endl;
-// std::cout << "Order made at " << ord::getRestaurant(order).name << std::endl;
-// std::cout << "Total price: R$" << ord::getTotalPrice(order) << std::endl;
