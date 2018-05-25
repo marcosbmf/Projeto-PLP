86ff99fe6d79b3f070abe95c34f79e73ace9f1a2
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2018-05-25 00:47:21 -0300

    iniciando menu do restaurante

---
+++ models/menuRestaurante.h
@@ -0,0 +1,12 @@
+#ifndef PLP_MENUCLIENTE_H
+#define PLP_MENUCLIENTE_H
+
+#endif //PLP_MENUCLIENTE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "item.h"
+#include "pedido.h"
+
