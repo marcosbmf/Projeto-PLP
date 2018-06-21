92e63870512826c12f0fa6eb05badb36619f0974
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2018-06-21 00:14:47 -0300

    fui comecar mas deu sono

---
+++ Haskell/delivery.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------
+-- 
+-- Sistema de Delivery em Haskell
+-- 
+-- Grupo -	José Ramon Fragoso da Silva - 116210412
+-- 			Marcos - 
+-- 			Matheus Eduardo - 
+-- 			Lucas Jhonanta - 
+-- 			Gustavo - 
+-----------------------------------------------------
+
+
+menuInicial::Int
+menuInicial = putStr "ola"
+
+-- menuRestaurante::Int
+-- menuRestaurante = 
+
+-- menuCliente::
+-- menuCliente = 
+
+main = do
+	menuInicial
