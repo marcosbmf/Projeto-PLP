33dfb29f205826e6911b8c9af6e5378142727067
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2018-07-05 16:44:46 -0300

    Adicionadas funções de cadastro, adição de pedidos e login.

--- src/Cliente.hs
+++ src/Cliente.hs
@@ -1,10 +1,14 @@
 {- Módulo com a definição do tipo Cliente e funções relacionadas a ele.
  
-   @author Gustavo
+   @author Gustavo, Marcos Barros
 -}
 module Cliente
 ( Cliente(..)
 , toString
+, novoCliente
+, cadastraPedido
+, loginCliente
+, cadastraCliente
 ) where
 
 {- Construtor do tipo Cliente.
@@ -21,6 +25,7 @@ data Cliente = Cliente { login :: String
                        , nome :: String
                        , cpf :: String
                        , endereco :: String
+                       , telefone :: String
                        , pedidos :: [String]
                        } deriving (Show)
                
@@ -34,7 +39,61 @@ data Cliente = Cliente { login :: String
 -}
 toString :: Cliente -> String
 toString clt = name ++ address ++ ssn ++ lgin
-               where name = "Nome: " ++ login clt ++ "\n"
+               where name = "Nome: " ++ nome clt ++ "\n"
                      address = "Endereço: " ++ endereco clt ++ "\n"
                      ssn = "CPF: " ++ cpf clt ++ "\n"
                      lgin = "Login: " ++ login clt ++ "\n"
+
+
+{- Cria um novo cliente a partir das entradas do usuário.
+Retorna IO(Cliente)
+-}
+novoCliente :: IO(Cliente)
+novoCliente = do putStrLn("Insira o seu nome:")
+                 nome <- getLine
+                 putStrLn("Insira o seu endereço:")
+                 endereco <- getLine
+                 putStrLn("Insira o seu telefone:")
+                 telefone <- getLine
+                 putStrLn("Insira o seu CPF:")
+                 cpf <- getLine
+                 putStrLn("Insira o seu login:")
+                 login <- getLine
+                 putStrLn("Insira a sua senha:")
+                 senha <- getLine
+                 let cliente = Cliente login senha nome cpf endereco telefone []
+                 return cliente
+
+
+{- Função para verificar se o login já existe. 
+
+Caso exista, retorna False, que significa que o cadastro é inválido.
+Caso não exista, o cadastro é válido, retorna True.
+-}
+verificaCadastro :: Cliente -> [Cliente] -> Bool
+verificaCadastro cliente [] = True
+verificaCadastro cliente [a] = False
+verificaCadastro cliente clientes = verificaCadastro cliente [y | y <- clientes, login y == (login cliente)]
+
+
+cadastraClienteAux :: [Cliente] -> IO(Either String [Cliente])
+cadastraClienteAux clientes = do cliente <- novoCliente
+                                 if verificaCadastro cliente clientes
+                                    then return (Right (clientes ++ [cliente]))
+                                    else return (Left "Login já existe, cadastro não realizado.")
+
+
+cadastraCliente :: [Cliente] -> IO([Cliente])
+cadastraCliente clientes = do eitherClientes <- cadastraClienteAux clientes
+                              case eitherClientes of
+                                    Right novoClientes -> return novoClientes
+                                    Left reason -> do putStrLn("Um cliente com o mesmo login foi detectado, cadastro não realizado.")
+                                                      return clientes
+
+cadastraPedido :: String -> Cliente -> Cliente
+cadastraPedido pedido cliente = Cliente (login cliente) (senha cliente) (nome cliente) (cpf cliente) (endereco cliente) (telefone cliente) ((pedidos cliente) ++ [pedido])
+
+loginCliente :: String -> String -> [Cliente] -> Either String Cliente
+loginCliente loginUser senhaUser [] = Left "Login e senha inválidos."
+loginCliente loginUser senhaUser [a] = Right a
+loginCliente loginUser senhaUser clientes = loginCliente (loginUser) (senhaUser) [y | y <- clientes, (login y) == (loginUser), (senha y) == (senhaUser)]
