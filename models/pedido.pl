a9a727ce656da874776b44a6658656425bc7a746
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2018-07-26 15:05:47 -0300

    criando gets e tostring de pedido, porem falta testar

--- models/pedido.pl
+++ models/pedido.pl
@@ -8,7 +8,9 @@
 %Pedido(loginCliente, CNPJRestaurante, ListaItems, ID(RANDOM)) -- A lista de items é em string, loginCliente e CNPJRestaurante como array de char.
 :- dynamic(pedido/4).
 
-novoPedido(Login, CNPJ, ListaItems) :-
+pedido([50], [50], "seilaaa", 4).
+
+newPedido(Login, CNPJ, ListaItems) :-
   getRandomNumber(CNPJ, ID),assert(pedido(Login, CNPJ, ListaItems, ID)).
 
 %Gera um identificador unico.
@@ -18,3 +20,25 @@ getRandomNumber(CNPJ, Result) :-
 %Verifica se o identificador é único.
 checkRandomUnico(CNPJ, ID) :-
   not(pedido(_,CNPJ,_,ID)).
+
+
+getClienteLogin(ID, StrLogin):-
+	pedido(Login,_,_,ID),
+	name(StrLogin, Login).
+
+getCNPJRestaurante(ID, StrCNPJ):-
+	pedido(_,CNPJ,_,ID),
+	name(StrCNPJ, CNPJ).
+
+getListaItems(ID, StrLista):-
+	pedido(_,_,LIsta',ID),
+	name(StrLista, Lista).
+
+
+toString(ID, Resultado) :-
+    getClienteLogin(ID, ClientePedido),
+    getCNPJRestaurante(ID, CNPJRestaurante),
+    getListaItems(ID, ListaItems),
+    format(atom(R), "-- Pedido --\n  Cliente: ~w - \nCNPJ do restaurante: ~w\nItens: ~w", [ClientePedido, CNPJRestaurante, ListaItems]),
+    Resultado = R.
+
